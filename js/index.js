21c66c00c4ce9d3f42d76a6ba5ca5fe929ec46ae
camila001 <camila.acuna.gut@gmail.com>
2020-10-13 16:49:55 -0300

    Creacion carpetas y archivos

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+var caja = document.getElementById('caja');
+
+document.getElementById('moneda').addEventListener('click',()=>{
+    caja.innerHTML = "Moneda";
+    
+});
+
+document.getElementById('pasteles').addEventListener('click',()=>{
+    caja.innerHTML = "Pasteles owo";
+    
+});
